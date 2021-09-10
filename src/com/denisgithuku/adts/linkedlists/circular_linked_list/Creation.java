aa1932a5dce18c74cef6f5ea87dbe1eaf2ff3fb7
DenisGithuku <https://github.com/DenisGithuku/AndroidKotlinFirebase.git>
2021-09-10 13:27:50 +0300

    create, insert, traverse a circular single linked list

--- src/com/denisgithuku/adts/linkedlists/circular_linked_list/Creation.java
+++ src/com/denisgithuku/adts/linkedlists/circular_linked_list/Creation.java
@@ -9,12 +9,66 @@ public class Creation {
     
     public static void main(String[] args) {
         Creation creation = new Creation();
-        creation.createCircularLinkedList();
+        creation.createCircularLinkedList(9);
+        System.out.println(creation.head.value);
+        creation.createCircularLinkedList(8);
+        System.out.println(creation.head.next.value);
+        creation.createCircularLinkedList(11);
+        System.out.println(creation.head.next.next.value);
+        creation.insertIntoCircularSinglyLinkedList(8, 0);
+        creation.insertIntoCircularSinglyLinkedList(23, 1);
+        creation.insertIntoCircularSinglyLinkedList(34, 2);
+        System.out.println(creation.head.next.next.value);
+        creation.traverseCircularSinglyLinkedList();
     }
     
-    public com.denisgithuku.adts.linkedlists.Node createCircularLinkedList(int nodeValue) {
-        com.denisgithuku.adts.linkedlists.Node node = new com.denisgithuku.adts.linkedlists.Node();
-        
+    public Node createCircularLinkedList(int nodeValue) {
+        head = new Node();
+        Node node = new Node();
+        node.next = node;
+        node.value = nodeValue;
+        head = node;
+        tail = node;
+        size = 1;
         return head;
+    }
+    
+    public void insertIntoCircularSinglyLinkedList(int nodeValue, int location) {
+        Node node = new Node();
+        node.value = nodeValue;
+        if (head == null) {
+            createCircularLinkedList(nodeValue);
+            return;
+        } else if (location == 0) {
+            node.next = head;
+            head = node;
+            tail.next = head;
+        } else if (location >= size) {
+            tail.next = node;
+            tail = node;
+            tail.next = head;
+        } else {
+            Node tempNode = head;
+            int index = 0;
+            while (index < location - 1) {
+                tempNode = tempNode.next;
+                index++;
+            }
+            node.next = tempNode.next;
+            tempNode.next = node;
+        }
+        size++;
+    }
+    
+    public void traverseCircularSinglyLinkedList() {
+        Node tempNode = head;
+        for (int i = 0; i < size; i++) {
+            System.out.print(tempNode.value);
+            if (i != size - 1) {
+                System.out.print(" -> ");
+            }
+            tempNode = tempNode.next;
+        }
+        System.out.print("\n");
     }
 }
