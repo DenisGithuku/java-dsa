b61d2456b8023aca6f2f3f9a6e8535ceebf4ccf8
DenisGithuku <https://github.com/DenisGithuku/AndroidKotlinFirebase.git>
2021-09-13 16:57:22 +0300

    doubly linked list

---
+++ src/com/denisgithuku/adts/linkedlists/doubly/doubly_linked_list/DoubleLinkedList.java
@@ -0,0 +1,18 @@
+package com.denisgithuku.adts.linkedlists.doubly.doubly_linked_list;
+
+import com.denisgithuku.adts.linkedlists.doubly.DoubleNode;
+
+public class DoubleLinkedList {
+    public DoubleNode doubleNode;
+    public DoubleNode head;
+    public DoubleNode tail;
+    
+    public static void main(String[] args) {
+        DoubleLinkedList dll = new DoubleLinkedList();
+    }
+    
+    public DoubleNode createDoubleLinkedList(int nodeValue) {
+        DoubleNode node = new DoubleNode();
+        return node;
+    }
+}
