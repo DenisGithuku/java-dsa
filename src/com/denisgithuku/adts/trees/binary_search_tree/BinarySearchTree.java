d0244789b9b99649e24630143cf7402525bdea02
DenisGithuku <https://github.com/DenisGithuku/AndroidKotlinFirebase.git>
2021-10-19 05:15:00 +0300

    insert node into a binary search tree

--- src/com/denisgithuku/adts/trees/binary_search_tree/BinarySearchTree.java
+++ src/com/denisgithuku/adts/trees/binary_search_tree/BinarySearchTree.java
@@ -5,10 +5,39 @@ public class BinarySearchTree {
     
     public static void main(String[] args) {
         BinarySearchTree binarySearchTree = new BinarySearchTree();
+        binarySearchTree.insert(70);
+        binarySearchTree.insert(80);
+        binarySearchTree.insert(90);
+        binarySearchTree.insert(30);
+        binarySearchTree.insert(60);
+        binarySearchTree.insert(80);
+        binarySearchTree.insert(100);
+        binarySearchTree.insert(20);
+        binarySearchTree.insert(40);
     }
     
     public BinarySearchTree() {
         this.root = null;
         System.out.println("Created binary search tree");
+    }
+    
+    //insert method
+    private BinarySearchNode insertNode(BinarySearchNode currentNode, int value) {
+        if (currentNode == null) {
+            BinarySearchNode newNode = new BinarySearchNode();
+            newNode.value = value;
+            System.out.println("Successfully inserted node");
+            return newNode;
+        } else if (value <= currentNode.value) {
+            currentNode.left = insertNode(currentNode.left, value);
+            return currentNode;
+        } else {
+            currentNode.right = insertNode(currentNode.right, value);
+            return currentNode;
+        }
+    }
+    
+    public void insert(int value) {
+        insertNode(root, value);
     }
 }
